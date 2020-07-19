26b932f5142bdfaaef3135ee7b130886a31dd22f
Benjamin Brehm <benjaminbrehm@Benjamins-MacBook-Pro.local>
2020-07-19 01:39:25 -0600

    fixes

--- query.py
+++ query.py
@@ -12,8 +12,18 @@ def main():
         dat = json.load(fil)
 
     for queen in dat:
-        #instagram_fol, instagram_lik = get_insta(dat[queen]['sm1'])
-        instagram_fol, instagram_lik = 1234, 2567
+        try:
+            instagram_fol, instagram_lik = get_insta(dat[queen]['sm1'])
+        except:
+            err_log('instagram global fault: %s' % queen)
+        try:
+            facebook_impr = get_facebook(dat[queen]['sm3'])
+        except:
+            err_log('facebook global fault: %s' % queen)
+        try:
+            twitter_fol, twitter_twts = get_twitter(dat[queen]['sm2'])
+        except:
+            err_log('twitter global fault: %s' % queen)
 
         extract = dat[queen]
 
@@ -21,10 +31,10 @@ def main():
             {
                 '_ts': datetime.now().strftime('%Y-%m-%dT%H:%M:%S'),
                 'insta_f': instagram_fol,
-                'twitr_f': get_twitter(dat[queen]['sm2']),
-                'faceb_f': get_facebook(dat[queen]['sm3']),
+                'twitr_f': twitter_fol,
+                'faceb_f': facebook_impr,
                 'sm4': 0,
-                'sm5': 0,
+                'twitr_t': twitter_twts,
                 'insta_l': instagram_lik
             }
         )
@@ -134,12 +144,38 @@ def get_facebook(username):
 
 
 def get_twitter(username):
-    return 0
+    if not username:
+        err_log('username null: %s' % username)
+        return 0, 0
+    if len(username) < 2:
+        err_log('username len fail: %s' % username)
+        return 0, 0
+    try:
+        r = requests.get('https://tweettunnel.com/%s' % username)
+    except:
+        err_log('request fail, https://tweettunnel.com/%s' % username)
+        return 0, 0
+
+    try:
+        follows = r.text.split('Following</span> &nbsp;&nbsp;')[1].split('<span')[0]
+        tweets = r.text.split('Followers</span> &nbsp;&nbsp;')[1].split('<span')[0]
+        follows = follows.strip().replace(',', '')
+        tweets = tweets.strip().replace(',', '')
+    except:
+        filen = os.path.join('logs', str(int(time.time())) + '.err')
+        err_log('chop fail: %s;  %s' % (username, filen))
+        with open(filen, 'w') as errr:
+            errr.write(r.text)
+        return 0, 0
+
+    return follows, tweets
 
 
-def err_log(str):
-    with open('err.log', 'a') as log:
-        log.write('%s\tERR: %s' % (datetime.now().strftime('%Y-%m-%dT%H:%M:%S'), str))
+def err_log(string):
+    fn = '%s.log' % datetime.now().strftime('%Y-%m-%d-%H')
+    erlog = os.path.join('logs', fn)
+    with open(erlog, 'a') as log:
+        log.write('%s\tERR: %s' % (datetime.now().strftime('%Y-%m-%dT%H:%M:%S'), string))
         log.write('\n')
     sys.stdout.write('E')
     sys.stdout.flush()
