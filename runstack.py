e84e2d0071425ae7c727173e1b6fd52abb6ad219
Benjamin Brehm <benjaminbrehm@Benjamins-MacBook-Pro.local>
2020-07-19 00:17:44 -0600

    new seasons

--- runstack.py
+++ runstack.py
@@ -6,7 +6,7 @@ import datetime
 
 class DragRace:
     def __init__(self):
-        with open('db.json', 'r') as fil:
+        with open('db2.json', 'r') as fil:
             self.dat = json.load(fil)
 
         self.smkeys = {
@@ -15,55 +15,34 @@ class DragRace:
             'facebook': 'sm3',
             'tumblrtag': 'sm4',
             'twittertag': 'sm5',
-            'sm6': 'sm6',
+            'instagram_likes_avg': 'sm6',
         }
 
     def save(self):
-        with open('db.json', 'w') as fil:
+        with open('db2.json', 'w') as fil:
             json.dump(self.dat, fil, sort_keys=True, indent=6)
 
-    def add_season(self, number, allstars=False):
-        if allstars:
-            szn = 'AS%d' % number
-        else:
-            szn = 'DR%d' % number
-
-        self.dat[szn] = {}
-        self.save()
-
-    def add_queen(self, name, szn, dob, eth, born, state, finish):
+    def add_queen(self, name, szn, finish, dob, eth, born, state):
         if dob < 1900:
             dob = datetime.datetime.now().year - dob
 
         newdat = {
-            'name': name,
-            'dob': dob,
-            'eth': eth,
-            'state': state,
-            'state_fmr': born,
-            'finish': finish,
-            'sm1': '',
-            'sm2': '',
-            'sm3': '',
-            'sm4': '',
-            'sm5': '',
-            'sm6': '',
+            "name": name,
+            "dob": dob,
+            "eth": eth,
+            "finish": [finish],
+            "seasons": [szn],
+            "sm1": "",
+            "sm2": "",
+            "sm3": "",
+            "sm4": "",
+            "sm5": "",
+            "sm6": "",
+            "state": state,
+            "state_fmr": born
         }
-        self.dat[szn][name.lower().replace(' ', '')] = newdat
-        self.save()
 
-    def add_sm(self, name, sm, payload):
-        for each in self.dat:
-            if name in self.dat[each]:
-                self.dat[each][name][self.smkeys.get(sm)] = payload
-                self.save()
-
-    def update_sm(self, old, new):
-        self.smkeys[new] = self.smkeys[old]
-        del self.smkeys[old]
-
-    def update_place(self, name, season, finish):
-        self.dat[season][name]['finish'] = finish
+        self.dat[name.lower().replace(' ', '')] = newdat
         self.save()
 
 
